267fd3c7ea3080e8739981b861cb490b911a9023
shubham Rawat <rawatshubam269@gmail.com>
2024-04-06 16:57:04 +0530

    Composer File Added

---
+++ src/components/composer.js
@@ -0,0 +1,37 @@
+import Blog from "./Blog/Blog";
+import Button from "./Button/Button";
+import Card from "./Card/Card";
+import DiscountSection from "./DiscountSection/DiscountSection";
+import DownloadSection from "./DownloadSection/DownloadSection";
+import FAQ from "./FAQ/FAQ";
+import Footer from "./Footer/Footer";
+import HeroSection from "./HeroSection/HeroSection";
+import HospitalDetailCard from "./HospitalDetailCard/HospitalDetailCard";
+import MedicalUtilities from "./MedicalUtilities/MedicalUtilities";
+import Navbar from "./Navbar/Navbar";
+import NotificationModal from "./NotificationModal/NotificationModal";
+import PatientCare from "./PatientCare/PatientCare";
+import SearchBox from "./SearchBox/SearchBox";
+import ServiceSection from "./ServiceSection/ServiceSection";
+import Slider from "./Slider/Slider";
+import Specialization from "./Specialization/Specialization";
+
+export {
+  Blog,
+  Button,
+  Card,
+  DiscountSection,
+  DownloadSection,
+  FAQ,
+  Footer,
+  HeroSection,
+  HospitalDetailCard,
+  MedicalUtilities,
+  Navbar,
+  NotificationModal,
+  PatientCare,
+  SearchBox,
+  ServiceSection,
+  Slider,
+  Specialization,
+};
